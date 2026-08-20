a2e748723901ec2e4f79c22b2263c68692e39c69
hackerbyadicto-afk <hackerbyadicto@gmail.com>
2026-08-20 16:57:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ def check(m,n,k):
     elif n=="%":
         return m*k/100
     else:
-        return "It is not available"
-code=gr.Interface(fn=check,inputs=[gr.Textbox(label="First number"),gr.Radio(["+","-","*",":","/","%"],label="Function"),gr.Textbox(label="Second number")],outputs=gr.Textbox(label="Result"),title="Calculator")
+        return "It is not available,now"
+code=gr.Interface(fn=check,inputs=[gr.Textbox(label="First number"),gr.Radio(["+","-","*","/","%"],label="Function"),gr.Textbox(label="Second number")],outputs=gr.Textbox(label="Result"),title="Calculator")
 code.launch(server_name="0.0.0.0",server_port=10000)
