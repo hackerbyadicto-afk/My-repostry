119b20981c072858c8b7d62290eb845023a5f5cd
hackerbyadicto-afk <hackerbyadicto@gmail.com>
2026-08-20 16:31:25 +0400

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def check(m,n,k):
     else:
         return "It is not available"
 code=gr.Interface(fn=check,inputs=[gr.Textbox(label="First number"),gr.Radio(["+","-","*",":","/","%"],label="Function"),gr.Textbox(label="Second number")],outputs=gr.Textbox(label="Result"),title="Calculator")
-code.launch()
+code.launch(server_name="0.0.0.0",server_port=10000)
